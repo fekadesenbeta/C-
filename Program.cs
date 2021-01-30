eecd5df94901a5b330a3a08447637d2de1fed008
Fekade Senbeta <fekadesenbeta@gmail.com>
2021-01-30 16:50:16 -0700

    first C# commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Print();
+        }
+        void Print()
+        {
+            Console.WriteLine("I am getting to learn C#!");
+            Console.WriteLine("It is fun");
+            Console.WriteLine("I like it!");
+        }
+    }
+}
